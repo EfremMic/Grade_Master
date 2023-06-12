33a42714332a3ab7b1fd36cf45d9724143243d9c
Efrem <100604322+EfremMic@users.noreply.github.com>
2023-06-12 12:51:16 +0200

    Update JDBCManager.java

--- src/GradeMaster/JDBCManager.java
+++ src/GradeMaster/JDBCManager.java
@@ -12,7 +12,7 @@ import java.util.List;
 public class JDBCManager {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/schoolgrademaster";
     private static final String DB_USERNAME = "root";
-    private static final String DB_PASSWORD = "Xpdy2222";
+    private static final String DB_PASSWORD = "*****";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
